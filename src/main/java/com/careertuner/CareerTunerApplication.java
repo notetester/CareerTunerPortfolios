dd70ef9613f68db70120e456ef88a113b99a5eb1
이정국 <nova4545@hanmail.net>
2026-06-05 00:47:22 +0900

    set

---
+++ src/main/java/com/careertuner/CareerTunerApplication.java
@@ -0,0 +1,13 @@
+package com.careertuner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerTunerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerTunerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/careertuner/CareerTunerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careertuner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerTunerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
